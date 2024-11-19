44f2c8e011f4644520896c1a594822d7bdeeb457
윤종민 <qerter20@gmail.com>
2024-11-19 15:23:09 +0900

    feat : 유저 모델 정의

--- pjt_backend/accounts/models.py
+++ pjt_backend/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Job(models.Model):
+    job_name = models.CharField(max_length=100)
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    birthday = models.DateField()
+    address = models.TextField()
+    servay_yn = models.BooleanField(default=False)
+    job_id = models.ForeignKey(Job, on_delete=models.SET_DEFAULT, default=0)
